068192e030940f4a0adf7147759097af90141dfd
ivsonsantos <Belgique@2024>
2021-05-19 13:27:43 +0200

    Service that create month and year for the combo

--- src/app/services/luv2-shop-form.service.ts
+++ src/app/services/luv2-shop-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,33 @@ import { Injectable } from '@angular/core';
 export class Luv2ShopFormService {
 
   constructor() { }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array for MOnth
+
+    for (let month = startMonth; month <= 12; month++) {
+      data.push(month);
+    }
+
+    return of(data); // .of operator from RJXJS will wrap an OBJECT as OBSERVABLE 
+  }
+
+  getCreditCardyears(): Observable<number[]> {
+
+    let data: number[] = [];
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let year = startYear; year <= endYear; year++) {
+      data.push(year);
+    }
+
+    return of(data);
+
+  }
+
 }
