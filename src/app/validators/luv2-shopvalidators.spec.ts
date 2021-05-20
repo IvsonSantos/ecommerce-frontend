febf59b9e39025d61d2034aaa127d4086ccf656c
ivsonsantos <Belgique@2024>
2021-05-20 11:16:20 +0200

    new class validator

---
+++ src/app/validators/luv2-shopvalidators.spec.ts
@@ -0,0 +1,7 @@
+import { Luv2Shopvalidators } from './luv2-shopvalidators';
+
+describe('Luv2Shopvalidators', () => {
+  it('should create an instance', () => {
+    expect(new Luv2Shopvalidators()).toBeTruthy();
+  });
+});
