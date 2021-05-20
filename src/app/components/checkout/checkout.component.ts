03f63c6a170ab7c3e0e56c63a8471c6bc6529429
ivsonsantos <Belgique@2024>
2021-05-20 10:26:24 +0200

    Form controls for validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/commom/country';
 import { State } from 'src/app/commom/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
@@ -31,9 +31,11 @@ export class CheckoutComponent implements OnInit {
     // cria o form
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', 
+          [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+        )
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
